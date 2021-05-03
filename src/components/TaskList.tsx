97370b79195d80d4bc2f497ff09d27028e7bea8a
rafaelst2000 <rafaelst2000@gmail.com>
2021-05-03 13:53:01 -0300

    completing challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const title = newTaskTitle.trim()
+    if (!title) return
+    const newTask: Task = {
+      id: Math.random(),
+      title,
+      isComplete: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggleds = tasks.map(task => {
+      return {
+        ...task,
+        isComplete: task.id === id ? !task.isComplete : task.isComplete
+      }
+    })
+    setTasks(toggleds)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filtereds = tasks.filter(task => task.id !== id)
+    setTasks(filtereds)
   }
 
   return (
@@ -32,9 +50,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +68,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +84,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
